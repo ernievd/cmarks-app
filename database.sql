a281790dd4125f25fee5feeda2178536b43431b7
Amy Richardson <amyrichardson96@gmail.com>
2018-03-05 07:46:10 -0600

    added sql file and updated db name in pool.js

---
+++ database.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+	id serial primary key,
+	auth_key  varchar(200) not null UNIQUE
+);
+
+CREATE TABLE events (
+	id serial primary key,
+	speaker_id int references users(id) not null,
+	speaker_name varchar(100) not null,
+	title varchar(100) not null,
+	location varchar(100),
+	date date not null,
+	start_time time not null,
+	join_code varchar(4),
+	audio_url varchar(240),
+	completed boolean default 'false'
+);
+
+CREATE TABLE cmarks (
+	id serial primary key,
+	user_id int references users(id) not null,
+	event_id int references events(id) not null,
+	timestamp time not null
+);
