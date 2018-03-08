2bf5739baa9ee9d16437e17d70ac17f71fa4d1bd
Ian Robertson <Ianmr08@gmail.com>
2018-03-08 11:03:52 -0600

    added swipe functionality

--- server/public/scripts/controllers/event.controller.js
+++ server/public/scripts/controllers/event.controller.js
@@ -1,6 +1,19 @@
 myApp.controller('EventController', ['UserService', 'EventService', 'CmarkService',
   function (UserService, EventService, CmarkService) {
+    console.log('EventController loaded');
+    
     var self = this;
+
     self.userService = UserService;
     self.userObject = UserService.userObject;
+
+    // Allows user to use swipe functionality on mobile (difficult on desktop)
+    self.onSwipeUp = function(ev, target) {
+      alert('You swiped up!!');
+      CmarkService.timestampSwipe();
+      console.log('Event Target: ', ev.target);
+      console.log('Event Current Target: ', ev.currentTarget);
+      console.log('Original Current Target: ', target.current);
+    };
+
   }]);
--- server/routes/cmark.router.js
+++ server/routes/cmark.router.js
@@ -1,4 +1,26 @@
+
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool');
+
+
+router.post('/swipe', (req, res) => {
+    console.log('req.body', req.body);
+    console.log('user', req.user);
+    let user_id = req.user.id;
+    let timestamp = req.body.actualTime;
+    const queryText = 'INSERT INTO cmarks (timestamp, user_id) VALUES ($1, $2)'; 
+
+    pool.query(queryText, [timestamp, user_id])
+    .then((result) => {
+        console.log('successful timestamp post', result);
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log('error on timestamp post', error);
+        res.sendStatus(500);
+    })
+});
+
 
 module.exports = router;
