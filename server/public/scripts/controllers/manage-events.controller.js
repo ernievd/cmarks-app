b66c7e1ebb06b92829a0a34a8ad8df3ce89a99cd
Amy Richardson <amyrichardson96@gmail.com>
2018-03-09 15:48:26 -0600

    added moment picker

--- server/public/scripts/controllers/manage-events.controller.js
+++ server/public/scripts/controllers/manage-events.controller.js
@@ -21,12 +21,19 @@ myApp.controller('ManageEventsController', ['UserService', 'EventService', 'Audi
         targetEvent: ev,
         clickOutsideToClose:true,
         fullscreen: self.customFullscreen // Only for -xs, -sm breakpoints.
-      })
-      // .then(function(answer) {
-      //   self.status = 'You said the information was "' + answer + '".';
-      // }, function() {
-      //   self.status = 'You cancelled the dialog.';
-      // });
+      });
+    };
+
+    self.showEditEvent = function(ev) {
+      $mdDialog.show({
+        controller: EditEventController,
+        controllerAs: 'vm',
+        templateUrl: '/../../views/templates/edit-event.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: self.customFullscreen // Only for -xs, -sm breakpoints.
+      });
     };
   
     function AddEventController($mdDialog, EventService) {
@@ -61,5 +68,41 @@ myApp.controller('ManageEventsController', ['UserService', 'EventService', 'Audi
         // close dialog
         $mdDialog.hide();
       }
+    }
+
+      function EditEventController($mdDialog, EventService) {
+        var self = this;
+        self.addEvent = EventService.addEvent;
+        
+  
+        self.hide = function() {
+          $mdDialog.hide();
+        };
+    
+        self.cancel = function() {
+          $mdDialog.cancel();
+        };
+  
+        self.submitEvent = function(newEvent){       
+          // format the start time to be saved in the database 
+          let start_time = moment(newEvent.start_time).format('h:mm:ss a');   
+          console.log('event: ', newEvent);
+          
+          // create event object with new start time
+          let eventToAdd = {
+            title: newEvent.title,
+            speaker_name: newEvent.speaker_name,
+            location: newEvent.location,
+            date: newEvent.date,
+            start_time: start_time 
+          }
+  
+          //send eventToAdd to the service to add to db
+          // EventService.addEvent(eventToAdd);
+          
+          // close dialog
+          $mdDialog.hide();
+        }
+
     }
 }]);
