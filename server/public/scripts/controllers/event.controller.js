331e6ad35be464341146022b1251f60c9cdca0ce
Ian Robertson <Ianmr08@gmail.com>
2018-03-12 10:55:51 -0500

    merging to dev

--- server/public/scripts/controllers/event.controller.js
+++ server/public/scripts/controllers/event.controller.js
@@ -1,36 +1,22 @@
 myApp.controller('EventController', ['UserService', 'EventService', 'CmarkService',
   function (UserService, EventService, CmarkService) {
     console.log('EventController loaded');
-
+    
     var self = this;
 
     self.userService = UserService;
     self.userObject = UserService.userObject;
-    self.eventInfo = EventService.eventInfo.list;
-    self.count = CmarkService.count;
-
-    self.count = 0;
+    self.eventInfo = EventService.eventInfo.list[0];
+    console.log('event', self.eventInfo);
+    
     // Allows user to use swipe functionality on mobile (difficult on desktop)
-    self.onSwipeUp = function (ev, target) {
-
-      swal("CMark Succesful!", {
-        buttons: false,
-        icon: "success",
-        timer: 1000,
-      });
-
-      // Counts Cmarks
-      self.count += 1;
-
-      // Records time every time cmark is made
-      CmarkService.timestampSwipe(self.eventInfo[0].id);
+    self.onSwipeUp = function(ev, target) {
+      alert('You swiped up!!');
+      CmarkService.timestampSwipe(self.eventInfo.id);
     };
 
-    // Re-routes user to the "my cmarks" page.
     self.finishEvent = function () {
       CmarkService.finishEvent();
     }
-
-
 
   }]);
