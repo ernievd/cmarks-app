bc3bd8ee3206c5bd9b93f4ff04da4737628e2e8a
Ian Robertson <Ianmr08@gmail.com>
2018-03-15 14:32:54 -0500

    added port

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use('/cmark', cmarkRouter);
 // Serve static files
 app.use(express.static('server/public'));
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 /** Listen * */
 app.listen(port, () => {
