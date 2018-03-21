6ab7a0939a2b2e7db7b6c61031321215ab5cd13f
Ross <rossdenison1@gmail.com>
2018-03-21 16:56:22 -0500

    added .env-dist file testing swipe up

--- server/public/scripts/controllers/event.controller.js
+++ server/public/scripts/controllers/event.controller.js
@@ -1,10 +1,6 @@
 myApp.controller('EventController', ['UserService', 'EventService', 'CmarkService', '$location',
 	function (UserService, EventService, CmarkService) {
 
-		// let preventOverscroll = require('prevent-overscroll');
-		// let domElement = document.getElementById('noOverscroll');
-		// let cleanup = preventOverscroll(domElement);
-
 		let self = this;
 
 		self.userService = UserService;
