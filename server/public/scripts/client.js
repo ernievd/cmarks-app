5198155757d5fa30ec2a8e17b824cb8f2c915c8c
Ross <rossdenison1@gmail.com>
2018-03-21 15:07:52 -0500

    one log from client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -20,7 +20,6 @@ myApp.config(function($mdThemingProvider) {
 
 /// Routes ///
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  console.log('myApp -- config')
   $routeProvider
     .when('/', {
       redirectTo: 'login'
